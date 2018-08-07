22da2faf46b89b2a10911b459b5cc7d94d340d2a
Garbo Cheng <garbocheng93@gmail.com>
2018-08-07 14:10:40 -0700

    Finish actions and reducers for User authentication

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,29 @@
+import * as API from "../util/session_api_util";
+
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+export const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const signup = formUser => dispatch => API.signup(formUser)
+  .then((user) => dispatch(receiveCurrentUser(user)));
+
+export const login = formUser => dispatch => API.login(formUser)
+  .then((user) => dispatch(receiveCurrentUser(user)));
+
+export const logout = () => dispatch => API.logout()
+  .then(() => dispatch(logoutCurrentUser()));
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionAPI from './util/session_api_util';
+import configureStore from './store/store';
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   window.logout = SessionAPI.logout;
   window.login = SessionAPI.login;
+  window.store = configureStore();
   ReactDOM.render(<h1>WELCOME!!!!!! </h1>, root);
 });
