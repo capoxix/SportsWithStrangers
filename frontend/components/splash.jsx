2c63bec30fb46fe3563d970ecbd43e37a51ac1f0
Garbo Cheng <garbocheng93@gmail.com>
2018-08-08 14:47:05 -0700

    Initial splash styling

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Splash = () => (
-  <div>
-    <div className="splash1">
-      <h2>Everyone is interesting</h2>
-      <p>But you don't discover that whhen you're starting at a sreen.</p>
-      <Link to='/'>LET'S PLAY SPORTS TOGETHER</Link>
+  <div className="splash">
+    <div className="row-1">
+      <div className="inner">
+        <h2>Everyone is interesting</h2>
+        <p>But you don't discover that whhen you're starting at a sreen.</p>
+        <Link to='/'>LET'S PLAY SPORTS TOGETHER</Link>
+      </div>
     </div>
-    <div className="splash2">
+    <div className="row-2">
       <div>
         <h2>Show up for sport time</h2>
         <p>You and a few others join a host at a sport's location</p>
@@ -22,7 +24,9 @@ const Splash = () => (
         <p> That's it. No strings attached.</p>
       </div>
     </div>
-    <div className="splash3">
+    <div className="row-3">
+      <div>
+      </div>
     </div>
 
   </div>
