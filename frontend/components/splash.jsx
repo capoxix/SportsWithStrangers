1040b1cb766b0336162307e1b715887484ecaee7
Garbo Cheng <garbocheng93@gmail.com>
2018-08-08 17:17:43 -0700

    Styling row4 of splash

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -32,20 +32,19 @@ const Splash = () => (
       <h3>SO WHY ARE THOUSANDS OF PEOPLE DOING IT?</h3>
 
       <div className="inner">
-        <div className='content'>
+        <img src={window.images.huddle}></img>
+        <div className='content-right'>
           <h2>It's weird.</h2>
-          <span>Everyone at tea time is stepping a little out of their
+          <span>Everyone at sport time is stepping a little out of their
             comfort zone. This makes it so much easier to actually learn
             something unexpected about the people around you.
             Because open minds are a prerequisite around here.
           </span>
         </div>
-        <img src={window.images.huddle}></img>
       </div>
 
       <div className="inner">
-        <img src={window.images.strangers}></img>
-        <div className='content'>
+        <div className='content-left'>
           <h2>We won't meet otherwise</h2>
           <span>In all likelihood, our paths won't cross for any reason.
             So often, we only meet people through work, school, or friends
@@ -53,20 +52,27 @@ const Splash = () => (
             we manufacture serendipity.
           </span>
         </div>
+        <img src={window.images.strangers}></img>
       </div>
 
       <div className="inner">
-        <div className='content'>
+        <img src={window.images.highFive}></img>
+        <div className='content-right'>
           <h2>Your hands are made for high fiving!</h2>
           <span>And your eyes are made…for eye contact!
             Real humans are so much cooler than their tweets or Instagram pictures.
             Sport time has real humans! #nofilter!
           </span>
         </div>
-        <img src={window.images.highFive}></img>
-
       </div>
 
+    </div>
+
+    <div className="row-4">
+      <div className="content">
+        <h2>Do we get to high five yet?</h2>
+        <Link to='/events'>YES</Link>
+      </div>
     </div>
 
   </div>
