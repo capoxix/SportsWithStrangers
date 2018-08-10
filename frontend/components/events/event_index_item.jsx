cd68a9852039771d868d6497ef99491e98ff42a3
Garbo Cheng <garbocheng93@gmail.com>
2018-08-10 15:01:15 -0700

    Further style event index item

--- frontend/components/events/event_index_item.jsx
+++ frontend/components/events/event_index_item.jsx
@@ -5,7 +5,10 @@ const EventIndexItem = (props) => {
   let date = new Date(props.event.date_time);
   let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   let day = days[date.getDay()];
-  let hour = date.getHours();
+  // let hour = date.getHours();
+  let hour = date.toLocaleString('en-US', { hour: 'numeric', hour12: true })
+
+
   let dateArr = date.toString().split(" ");
   //need to modify spots left after worked on join events
   return(
@@ -14,9 +17,9 @@ const EventIndexItem = (props) => {
       <div className='date-user-wrapper'>
         <div className='date-wrapper'>
           <div className='category'>{props.event.category_id}</div>
-          <div>{day}</div>
-          <div>{`${dateArr[1]} ${dateArr[2]}`}</div>
-          <div>{hour}</div>
+          <div className= 'day'>{day}</div>
+          <div className='date'>{`${dateArr[1]} ${dateArr[2]}`}</div>
+          <div className='hour'>{hour}</div>
         </div>
         <div className= 'user'>{props.user.name}</div>
       </div>
