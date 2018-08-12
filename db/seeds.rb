0c8811cf0a8d8a4b52db985c316cc86b5488907c
Garbo Cheng <garbocheng93@gmail.com>
2018-08-12 15:51:45 -0700

    Building seeds files

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 User.destroy_all
 
 garbo = User.create!(
-  name: "Garbo Cheng",
+  name: "Garbo",
   password: "123456",
   email: "garbocheng93@gmail.com",
   user_catchphrase: "If you like to play basketball with a team player, I'm your guy!",
@@ -17,15 +17,24 @@ garbo = User.create!(
 )
 
 lebron = User.create!(
-  name: "Lebron James",
+  name: "Lebron",
   password: "123456",
   email: "lebronjames@gmail.com",
   user_catchphrase: "Have you ever wondered what's it like to play basketball with a king?",
   user_description: "Ended Cleveland's 52 years championship drought. Ending Lakers 5 years playoff drought."
 )
 
+curry = User.create!(
+  name: "Stephen",
+  password: "123456",
+  email: "scurry30@gmail.com",
+  user_catchphrase: "You ever wonder what's it like to defend someone who
+    can make half court shots like layups?",
+  user_description: "One of the splash brothers."
+)
+
 roger = User.create!(
-  name: "Roger Federer",
+  name: "Roger",
   password: "123456",
   email: "rogerfederer@yahoo.com",
   user_catchphrase: "Tick Tock Tick Tock, time is running and so is your chance to play with a legend.",
@@ -33,12 +42,30 @@ roger = User.create!(
 )
 
 serena = User.create!(
-  name: "Serena Williams",
+  name: "Serena",
   password: "123456",
   email: "serenawilliams@hotmail.com",
   user_catchphrase: "You like watching me play on TV? Would you like to play against me?",
   user_description: "The queen of tennis."
 )
+
+messi = User.create!(
+  name: "Messi",
+  password: "123456",
+  email: "messi@hotmail.com",
+  user_catchphrase: "Would you like to learn how to get up everytime you fall?",
+  user_description: "The men who became a superstar football player despite the odds"
+)
+
+beckam = User.create!(
+  name: "Beckham",
+  password: "123456",
+  email: "messi@hotmail.com",
+  user_catchphrase: "Would you like to learn how to get up everytime you fall?",
+  user_description: "First English player to win league title in four countries: England,
+  Spain, the United States and France."
+)
+
 
 la = City.create!(name: "Los Angeles")
 sf = City.create!(name: "San Francisco")
@@ -47,4 +74,3 @@ oakland = City.create!(name: "Oakland")
 bball = Category.create!(name: "Basketball")
 soccer = Category.create!(name: "Soccer")
 tennis = Category.create!(name: "Tennis")
-badminton = Category.create!(name: "Badminton")
