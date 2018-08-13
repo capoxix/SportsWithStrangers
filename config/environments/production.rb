aba06a4aaa910ba91a120e70e103e050d62189c7
Garbo Cheng <garbocheng93@gmail.com>
2018-08-12 19:34:33 -0700

    Change production.rb to use amazon_dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 
-  config.active_storage.service = :amazon_prod
+  config.active_storage.service = :amazon_dev
   # Code is not reloaded between requests.
   config.cache_classes = true
 
