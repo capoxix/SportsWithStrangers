ab4e37a46b08f098ef19974d6aeca5fce85e2f36
Garbo Cheng <garbocheng93@gmail.com>
2018-08-14 15:25:38 -0700

    further style index

--- frontend/components/events/event_index.jsx
+++ frontend/components/events/event_index.jsx
@@ -43,10 +43,29 @@ class EventIndex extends React.Component{
         );
       }
 
+
     );
+    let date = new Date();
+    let monthArr = ["January", "February", "March", "April", "May",
+  "June", "July", "August", "September", "October", "November", "December"];
+    let month = monthArr[date.getMonth()];
+    let nextMonth = monthArr[date.getMonth() + 1];
+    let day = 30 - date.getDate();
       return(
         <div>
           <div className= 'index-container'>
+            <div className="month-info">
+              <div className="current-month">
+                <div className="emoji"><img src={window.images.calendarIcon}></img></div>
+                  <p>TEA TIMES IN {month}</p>
+                </div>
+
+                <div className="days-left">
+                  <p>{`${nextMonth}'s SPORTS TIME AVAILABLE IN ${day} DAYS`}</p>
+                </div>
+              </div>
+
+
             <Cities cities={this.props.cities}/>
             <div className='index-wrapper'>
               {citiesEvent}
