1218d2b1ed65d65d8e17c3b68bafef7437133e40
Garbo Cheng <garbocheng93@gmail.com>
2018-08-15 10:02:46 -0700

    create selectors for dashboard

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -8,3 +8,30 @@ export const getFilteredEvents = ({events, cityId}) => {
   return result;
   //array of events where city matches given city
 };
+
+export const getHostedEvents = ({events, currentUser}) => {
+  let result = [];
+  for (let id in events){
+    if(events[id].user_id === currentUser.id) result.push(events[id]);
+  }
+
+  return result;
+};
+
+export const getJoinedEvents = ({events, currentUser}) => {
+  let result = [];
+  for (let id in events){
+    if(currentUser.attending_event_ids.includes(events[id].id))
+    result.push(events[id]);
+  }
+  return result;
+};
+
+export const getWaitlistedEvents = ({events, currentUser}) => {
+  let result = [];
+  for (let id in events){
+    if(currentUser.waiting_event_ids.includes(events[id].id))
+    result.push(events[id]);
+  }
+  return result;
+};
